efce44caddeaed5831fadf890a8337489c43e951
LeeJeKyoung <jkl@hunesion.com>
2021-08-17 15:01:23 +0900

    Create Event.h : event defination

---
+++ nac_client/src/core/Event.h
@@ -0,0 +1,11 @@
+#ifndef _HUNE_CORE_EVENT_H_
+#define _HUNE_CORE_EVENT_H_
+
+#define HUNE_CORE_DESKTOP_NOTIFICATION "Hune::Core::DesktopNotification"
+#define HUNE_CORE_MESSAGE_BOX "Hune::Core::MessageBox"
+
+#define HUNE_CORE_IONEX_LOGIN "Hune::Core::IonexLogin"
+#define HUNE_CORE_IONEX_LOGOUT "Hune::Core::IonexLogout"
+
+
+#endif /* _HUNE_CORE_EVENT_H_ */
