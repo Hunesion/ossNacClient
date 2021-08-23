cb7281a7ee9abafcda8a20e825e320444ecaeea4
LeeJeKyoung <jkl@hunesion.com>
2021-08-23 15:00:01 +0900

    Create Event List

---
+++ nac_client/src/ui/Event.h
@@ -0,0 +1,8 @@
+#ifndef _HUNE_UI_EVENT_H_
+#define _HUNE_UI_EVENT_H_
+
+
+/////////////// OTHER UI EVENT ///////////////
+#define NAC_UI_APPLICATION_QUIT                 "Nac::Ui::AppicationQuit"
+
+#endif /* _FTC_UI_EVENT_H_ */
