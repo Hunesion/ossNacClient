09c6f53f9edd39c61fe4720fb46a6ae79b2820ab
hns10109 <iychoi@hunesion.com>
2021-11-16 12:01:45 +0900

    delete comment in main.cpp

--- nac_client/src/main.cpp
+++ nac_client/src/main.cpp
@@ -1,17 +1,17 @@
 #include <gtk/gtk.h>
 #include "ui/nacapplication.h"
 
-// USING_FTC_CORE ; 
 
 int main(int argc , char **argv){
     int rv = 0 ; 
 
     NacApplication *app = NULL ; 
-    //HUNE_LOG("Main Application Started"); 
+	
     app = nac_application_new(); 
     if (app == NULL) return rv ; 
     rv =  g_application_run(G_APPLICATION (app), argc, argv);
+	
     g_object_unref(app); 
-    //HUNE_LOG("Main Application End"); 
+
     return rv ; 
 }
